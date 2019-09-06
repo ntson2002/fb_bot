715f8ff0a4401f16f5142f6dc9df8c18bb9b6c04
Nguyen Son <sonnguyen@sonnt.local>
2019-09-06 13:33:42 +0700

    update

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ app.register_blueprint(routes)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    ACCESS_TOKEN = 'EAAh51ZAZCrmYIBAHUqYn8pBlJKvJEkA2sbZBx2oQEUk4uZARv8HDtF3o91z5ZBxeS5n8FChbRZB6WsLKZCb3n3QS6yH9TGSFJIe5UPciCpqVZBmqcZBqj8y7cZCwqr5uljzZAHaVPk5yppxDwiWpX2UyTnjw6LHaq78fWf3HTRT77EIxQZDZD'
+    return ACCESS_TOKEN
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
